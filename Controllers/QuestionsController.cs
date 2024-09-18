788cd87f430cab9fc5c03ef59da9de82423a1219
Your Name <laura.e.knoblock@gmail.com>
2024-09-18 12:38:22 -0500

    initial commit

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheMutants.Controllers
+{
+    public class QuestionsController : Controller
+
+
+    {
+
+        private static List<string> Questions = new List<string>();
+
+        public IActionResult Index()
+        {
+
+            ViewBag.questions = Questions;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Questions/Add")]
+
+        public IActionResult NewQuestion(string name)
+        {
+            Questions.Add(name);
+
+            return Redirect("/Questions");
+        }
+    }
+}
